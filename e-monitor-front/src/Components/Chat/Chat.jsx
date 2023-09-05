aadd13dc735c247e3da29908b3ec826627a4eb85
wal-wizard <goneisgone@hotmail.com>
2023-09-05 00:02:28 -0300

    chat

--- e-monitor-front/src/Components/Chat/Chat.jsx
+++ e-monitor-front/src/Components/Chat/Chat.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Chat.css";
+import Nav from "../Nav/Nav"
+
+//Icons
+import { AiOutlineSend } from "react-icons/ai";
+import { BsFiles } from "react-icons/bs";
 
 const Chat = () => {
     return (
-        <div className="chatWindow">
-
+        <div className="chat-component">
+            <Nav />
+            <div className="chat--window">
+                <div className="chat"></div>
+                <div className="caht--option">
+                    <BsFiles className="icon-clip"/>
+                    <input type="search" className="input-text"/>
+                    <button className="icon-send">
+                    <AiOutlineSend  className="ic"/>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
