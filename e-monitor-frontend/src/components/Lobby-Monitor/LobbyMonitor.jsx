3ff9f1888f920f79ed6ad84b675718cbb67c8a65
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-09-26 14:27:40 -0300

    update

--- e-monitor-frontend/src/components/Lobby-Monitor/LobbyMonitor.jsx
+++ e-monitor-frontend/src/components/Lobby-Monitor/LobbyMonitor.jsx
@@ -59,7 +59,7 @@ const LobbyMonitor = () => {
                 },
             })
 
-            handleGetTicket();
+            handleGetTicket();                                                                                                                                                                                     
         } catch (error) {
             console.error(error)
         }
