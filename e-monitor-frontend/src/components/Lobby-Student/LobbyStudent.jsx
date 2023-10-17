6ea46b145a4f912ac10d07bab5ebe242d4cb4897
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-10-17 15:24:50 -0300

    renomear ticket quase pronto

--- e-monitor-frontend/src/components/Lobby-Student/LobbyStudent.jsx
+++ e-monitor-frontend/src/components/Lobby-Student/LobbyStudent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './LobbyStudent.scss';
 
+import { BiSolidEdit } from "react-icons/bi";
 import { AiOutlineReload } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { BsExclamationCircleFill } from "react-icons/bs"
@@ -11,8 +12,12 @@ const LobbyStudent = () => {
     const navigate = useNavigate();
     const TOKEN = localStorage.getItem('token');
     const URL = 'https://emonitor-tsa0.onrender.com/api/v1/tickets/get-tickets'
+    const TICKETID = localStorage.getItem('ticketId');
 
     const [ticketInfo, setTicketInfo] = useState([])
+    const [title, setTitle] = useState("");
+    const [isRenaming, setIsRenaming] = useState(false);
+    const [renamingTicketId, setRenamingTicketId] = useState(null);
 
     useEffect(() => {
         handleGetTicket();
@@ -34,6 +39,28 @@ const LobbyStudent = () => {
             } else {
                 console.error('Erro na solicitação:', response.status);
             }
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleTitleChange = (e) => {
+        setTitle(e.target.value);
+    };
+
+    const renameTicket = async () => {
+        try {
+            await fetch(`https://emonitor-tsa0.onrender.com/api/v1/tickets/rename-ticket`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + TOKEN
+                },
+                body: JSON.stringify({
+                    id: TICKETID,
+                    newSubject: title
+                }),
+            })
         } catch (error) {
             console.error(error)
         }
@@ -71,14 +98,56 @@ const LobbyStudent = () => {
 
                                         {/*<img src={ticket.userImage} alt="User" id='user-img' />*/}
                                         <div className="ticket-info">
-                                            <h3 className="ticket-title">{ticket.subject}</h3>
+                                            {
+                                                isRenaming && renamingTicketId === ticket.id ? (
+                                                    <input 
+                                                        className="input-title"
+                                                        type="text" 
+                                                        placeholder= {ticket.subject}
+                                                        value={title} 
+                                                        onChange={handleTitleChange}
+                                                        onBlur={() => {
+                                                            setIsRenaming(false);
+                                                            setRenamingTicketId(null);
+                                                        }}
+                                                        onKeyDown={(e) => {
+                                                            if (e.key === 'Enter') {
+                                                                renameTicket();
+                                                                setIsRenaming(false);
+                                                            }
+                                                        }}
+                                                        onClick={(e) => { e.stopPropagation()}}
+                                                        autoFocus
+                                                    />
+                                                ) : (
+                                                    <h3 className="ticket-title">{ticket.subject}</h3>
+                                                )
+                                            }
+                                            
                                             <div className="ticket-date">
-                                                {/*new Date(ticket.date).toLocaleDateString('pt-BR', {
-                                        day: '2-digit',
-                                        month: '2-digit'
-                                    })*/}
+                                            {/*new Date(ticket.date).toLocaleDateString('pt-BR', {
+                                                day: '2-digit',
+                                                month: '2-digit'
+                                            })*/}
                                             </div>
                                         </div>
+
+                                        <button
+                                            className='action-btn'
+                                            onClick={
+                                                (e) => {
+                                                    //localStorage.setItem("ticketId", ticket.id);
+                                                    setRenamingTicketId(ticket.id);
+                                                    setTitle("");
+                                                    e.stopPropagation();
+                                                    setIsRenaming(true);
+                                                }
+                                            }
+
+                                            aria-describedby='claim'
+                                        >
+                                            <BiSolidEdit className="action-icon" />
+                                        </button>
 
                                         <div className="ticket-read-icon">
                                             {ticket.status === "OPEN" ? <BsExclamationCircleFill className='exclamation-icons' /> : ""}
