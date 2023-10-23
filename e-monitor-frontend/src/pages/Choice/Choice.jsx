ddd3f45b026898b1f1c7ad718da8d0a74ca11154
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-10-23 14:41:18 -0300

    Botão de voltar no cadastro.

--- e-monitor-frontend/src/pages/Choice/Choice.jsx
+++ e-monitor-frontend/src/pages/Choice/Choice.jsx
@@ -1,14 +1,33 @@
 import React from "react";
 import "./Choice.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 //import {MdSchool} from "react-icons/md/index"
 import { IoMdSchool } from "react-icons/io"
 import { PiStudentBold } from "react-icons/pi"
+import { MdOutlineArrowBack } from "react-icons/md";
 
 const Choice = () => {
+
+    const navigate = useNavigate();
+
     return (
         <div className="choice--container">
+
+            <button
+                className='action-btn'
+                onClick={
+                    (e) => {
+                        e.preventDefault();
+                        navigate('/', { replace: true })
+                    }
+                }
+
+                aria-describedby='claim'
+            >
+                <MdOutlineArrowBack className="action-icon" />
+            </button>
+
             <div className="container">
                 <img
                     src="./Images/logo.png"
