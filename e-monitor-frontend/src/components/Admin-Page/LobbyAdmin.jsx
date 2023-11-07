291d3ab6670e4adfbcd03ec40936ed105ea1ae76
Alisson Costa Schmidt <acschmidt@inf.ufsm.br>
2023-11-06 22:52:00 -0300

    Update LobbyAdmin

--- e-monitor-frontend/src/components/Admin-Page/LobbyAdmin.jsx
+++ e-monitor-frontend/src/components/Admin-Page/LobbyAdmin.jsx
@@ -5,6 +5,9 @@ import { URL } from '../../scripts/scripts';
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 
+// Components
+import Nav from '../Nav/Nav';
+
 // Icons
 import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
 import { MdOutlinePersonRemove, MdOutlinePlaylistRemove } from "react-icons/md";
@@ -12,162 +15,244 @@ import { MdOutlinePersonRemove, MdOutlinePlaylistRemove } from "react-icons/md";
 function AdminPage() {
     const navigate = useNavigate();
 
-    const URL_Get = URL + '/api/v1/tickets/get-tickets'
-    const TOKEN = localStorage.getItem('token');
-    const TICKETID = localStorage.getItem('ticketId');
+    const URL_GetSolicitations = URL + '/api/v1/admin/get-monitor-solicitations'
+    const URL_Approvemonitor = URL + '/api/v1/admin/approve-monitor'
+    const URL_RejectMonitor = URL + '/api/v1/admin/reject-monitor'
 
-    const [onSolicitacoes, setOnSolicitacoes] = useState(true);
-    const [ticketInfo, setTicketInfo] = useState([])
+    const URL_GetReports = URL + '/api/v1/admin/get-reports'
+    const URL_AcceptReport = URL + '/api/v1/admin/accept-report'
+    const URL_RejectReport = URL + '/api/v1/admin/reject-report'
 
-    const [solicitacoes, setSolicitacoes] = useState([]);
-    const [denuncias, setDenuncias] = useState([]);
+    const [solicitations, setSolicitations] = useState([]);
+    const [reports, setReports] = useState([]);
+
+    const token = localStorage.getItem('token');
+
+    const [onSolicitations, setOnSolicitations] = useState(true);
 
     useEffect(() => {
-        handleGetTicket();
-        //handleGetSolicitacoes();
-        //handleGetDenuncias();
+        handleGetSolicitations();
+        handleGetReports();
     }, []);
 
-    const handleGetTicket = async () => {
+    const handleGetSolicitations = async () => {
         try {
-            const response = await fetch(URL_Get, {
+            const response = await fetch(URL_GetSolicitations, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
-                    'Authorization': "Bearer " + TOKEN
+                    'Authorization': "Bearer " + token
                 }
             })
 
-            if (response.ok) {
+            try {
                 const data = await response.json();
-                setTicketInfo(data)
-            } else {
-                console.error('Erro na solicitação:', response.status);
+                setSolicitations(data)
+            } catch (error) {
+                console.log(error);
             }
         } catch (error) {
-            console.error(error)
+            console.log(error);
+        }
+    }
+
+    const handleGetReports = async () => {
+        try {
+            const response = await fetch(URL_GetReports, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + token
+                }
+            })
+
+            try {
+                const data = await response.json();
+                setReports(data)
+            } catch (error) {
+                console.log(error);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleApproveMonitor = async (e, userId) => {
+        e.preventDefault();
+
+        try {
+            await fetch(URL_Approvemonitor, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + token
+                },
+                body: JSON.stringify({
+                    userId : userId 
+                })
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleRejectMonitor = async (e, userId) => {
+        e.preventDefault();
+
+        try {
+            await fetch(URL_RejectMonitor, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + token
+                },
+                body: JSON.stringify({
+                    userId : userId 
+                })
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleAcceptReport = async (e, reportId ) => {
+        e.preventDefault();
+
+        try {
+            await fetch(URL_AcceptReport, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + token
+                },
+                body: JSON.stringify({
+                    reportId : reportId 
+                })
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const handleRejectReport = async (e, reportId ) => {
+        e.preventDefault();
+
+        try {
+            await fetch(URL_RejectReport, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + token
+                },
+                body: JSON.stringify({
+                    reportId : reportId 
+                })
+            })
+        } catch (error) {
+            console.log(error);
         }
     }
 
     return (
         <div className="admin-page">
+            < Nav />
             <h1 className='page--title'>Admin Page</h1>
 
             <div className='swap-page'>
-                <button className='button' onClick={() => setOnSolicitacoes(true)}>Solicitações</button>
-                <button className='button' onClick={() => setOnSolicitacoes(false)}>Denuncias</button>
+                <button className='button' onClick={() => setOnSolicitations(true)}>Solicitações</button>
+                <button className='button' onClick={() => setOnSolicitations(false)}>Denúncias</button>
             </div>
 
             {
-                onSolicitacoes ? (
-                    ticketInfo.length === 0 ? (
-                        <div className="no-tickets">
+                onSolicitations ? (
+                    solicitations.length === 0 ? (
+                        <div className="no-solicitation">
                             <h1>Ainda não há Solicitações!</h1>
                         </div>
                     ) : (
-                        ticketInfo.map((ticket, index) => (
-                            <div
-                                className="ticket"
-                                key={index}
-                                onClick={
-                                    () => {
-                                        localStorage.setItem("ticketId", ticket.id);
-                                        navigate('/chat', { replace: false })
-                                    }
-                                }
-                            >
-                                <div className="ticket-info">
-                                    <h3 className="ticket-title">{ticket.subject}</h3>
-                                    <p className="ticket-status">{ticket.topicId}</p>
-                                </div>
+                        < div className="solicitations-list" >
+                            {
+                                solicitations.map((solicitation, index) => (
+                                    <div
+                                        className="solicitation"
+                                        key={index}
+                                    >
+                                        <div className="solicitation-info">
+                                            <h3 className="solicitation-title">{solicitation.name}</h3>
+                                            <p className="solicitation-status">{solicitation.email}</p>
+                                        </div>
 
-                                <button
-                                    className='action-btn'
-                                    onClick={
-                                        (e) => {
-                                            e.stopPropagation();
-                                            localStorage.setItem("ticketId", ticket.id);
-                                            claimTicket()
-                                        }
-                                    }
+                                        <button
+                                            className='action-btn'
+                                            onClick={
+                                                (e) => {
+                                                    handleApproveMonitor(e, solicitation.id);
+                                                }
+                                            }
+                                        >
+                                            <AiOutlineCheck className='action-icon' />
+                                        </button>
 
-                                    aria-describedby='claim'
-                                >
-                                    <AiOutlineCheck className='action-icon' />
-                                </button>
+                                        <button
+                                            className='action-btn'
+                                            onClick={
+                                                (e) => {
+                                                    handleRejectMonitor(e, solicitation.id);
+                                                }
+                                            }
+                                        >
+                                            <AiOutlineClose className='action-icon' />
+                                        </button>
 
-                                <button
-                                    className='action-btn'
-                                    onClick={
-                                        (e) => {
-                                            e.stopPropagation();
-                                            localStorage.setItem("ticketId", ticket.id);
-                                            claimTicket()
-                                        }
-                                    }
-
-                                    aria-describedby='claim'
-                                >
-                                    <AiOutlineClose className='action-icon' />
-                                </button>
-
-                            </div>
-                        ))
+                                    </div>
+                                ))
+                            }
+                        </div >
                     )
                 ) : (
-                    ticketInfo.length === 0 ? (
+                    reports.length === 0 ? (
                         <div className="no-tickets">
-                            <h1>Ainda não há Solicitações!</h1>
+                            <h1>Oba! Sem denúncias!</h1>
                         </div>
                     ) : (
-                        ticketInfo.map((ticket, index) => (
-                            <div
-                                className="ticket"
-                                key={index}
-                                onClick={
-                                    () => {
-                                        localStorage.setItem("ticketId", ticket.id);
-                                        navigate('/chat', { replace: false })
-                                    }
-                                }
-                            >
-                                <div className="ticket-info">
-                                    <h3 className="ticket-title">{ticket.subject}</h3>
-                                    <p className="ticket-status">{ticket.topicId}</p>
-                                </div>
-    
-                                <button
-                                    className='action-btn'
-                                    onClick={
-                                        (e) => {
-                                            e.stopPropagation();
-                                            localStorage.setItem("ticketId", ticket.id);
-                                            claimTicket()
-                                        }
-                                    }
-    
-                                    aria-describedby='claim'
-                                >
-                                    <MdOutlinePersonRemove className='action-icon' />
-                                </button>
-    
-                                <button
-                                    className='action-btn'
-                                    onClick={
-                                        (e) => {
-                                            e.stopPropagation();
-                                            localStorage.setItem("ticketId", ticket.id);
-                                            claimTicket()
-                                        }
-                                    }
-    
-                                    aria-describedby='claim'
-                                >
-                                    <MdOutlinePlaylistRemove className='action-icon' />
-                                </button>
-    
-                            </div>
-                        ))
+                        < div className="solicitations-list" >
+                            {
+                                reports.map((report, index) => (
+                                    <div
+                                        className="solicitation"
+                                        key={index}
+                                    >
+                                        <div className="solicitation-info">
+                                            <h3 className="solicitation-title">Report</h3>
+                                            <p className="solicitation-status">{report.id}</p>
+                                        </div>
+
+                                        <button
+                                            className='action-btn'
+                                            onClick={
+                                                (e) => {
+                                                    handleAcceptReport(e, report.id)
+                                                }
+                                            }
+                                        >
+                                            <MdOutlinePersonRemove className='action-icon' />
+                                        </button>
+
+                                        <button
+                                            className='action-btn'
+                                            onClick={
+                                                (e) => {
+                                                    handleRejectReport(e, report.id)
+                                                }
+                                            }
+                                        >
+                                            <MdOutlinePlaylistRemove className='action-icon' />
+                                        </button>
+
+                                    </div>
+                                ))
+                            }
+                        </div >
                     )
                 )
             }
